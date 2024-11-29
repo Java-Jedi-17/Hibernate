6d67910ff536f00ab4eef5fae7bcf99cff2d1418
JavaJedi <aleksk170400@gmail.com>
2024-11-30 03:22:46 +0500

    Initial commit

--- src/main/java/ru/netology/hibernate/repository/Repository.java
+++ src/main/java/ru/netology/hibernate/repository/Repository.java
@@ -1,4 +1,18 @@
 package ru.netology.hibernate.repository;
 
+import ru.netology.hibernate.entity.Person;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@org.springframework.stereotype.Repository
 public class Repository {
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Person> getPersonsByCity(String city) {
+        return manager.createQuery("SELECT p FROM Person p WHERE p.city_of_living = :city")
+                .setParameter("city",city).getResultList();
+    }
 }
